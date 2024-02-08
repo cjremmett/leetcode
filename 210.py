f3aa6a93b8bc1de0d054ec74f7ba45a231cb7d5f
cjremmett <cjremmett@gmail.com>
2024-02-08 14:42:40 -0500

    finished 210

--- 210.py
+++ 210.py
@@ -5,14 +5,34 @@ class Solution:
         self.adjacency_list = {i:set() for i in range(0, numCourses)}
         
         for prereq in prerequisites:
-            self.adjacency_list[prereq[1]].add(prereq[0])
+            self.adjacency_list[prereq[0]].add(prereq[1])
 
-        print(self.adjacency_list)
+        self.course_order = []
+        self.visited = set()
+        self.circular = False
 
-        return []
+        for course in self.adjacency_list:
+            self.topological_traverse(course, set())
+         
+        if self.circular == True:
+            return []
+        else:
+            return self.course_order
     
+    def topological_traverse(self, course, already_seen):
+        if course in already_seen:
+            self.circular = True
+            return
+        elif course in self.visited or self.circular == True:
+            return
+        else:
+            already_seen.add(course)
+            self.visited.add(course)
+            for prereq in self.adjacency_list[course]:
+                self.topological_traverse(prereq, already_seen.copy())
+            self.course_order.append(course)
 
 if __name__ == '__main__':
-    test_cases = ( (2, [[1,0]], [0,1]), (4, [[1,0],[2,0],[3,1],[3,2]], [0,2,1,3]) )
+    test_cases = ( (3, [[0,2],[1,2],[2,0]], []), (2, [[1,0],[0,1]], []), (3, [[0,1],[0,2],[1,2]], [2,1,0]), (2, [[1,0]], [0,1]), (4, [[1,0],[2,0],[3,1],[3,2]], [0,2,1,3]) )
     for case in test_cases:
         print('Input: ' + str(case[0]) + ' ' + str(case[1]) + '\nExpected Output: ' + str(case[2]) + '\nActual Output: ' + str(Solution().findOrder(case[0], case[1])) + '\n')
