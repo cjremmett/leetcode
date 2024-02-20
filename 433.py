8c024d1e3edd5bbd559a13965daf0b42d6ca0fa8
cjremmett <cjremmett@gmail.com>
2024-02-20 17:10:51 -0500

    some work on 433

--- 433.py
+++ 433.py
@@ -6,12 +6,34 @@ class Solution:
         self.adjacency_list = {gene:set() for gene in bank}
         self.adjacency_list[startGene] = set()
         self.gene_chars = ['A', 'C', 'G', 'T']
-        for gene in bank:
+        for gene in self.adjacency_list:
             for i in range(0, 8):
                 for char in self.gene_chars:
                     temp_gene = gene[0:i] + char + gene[i + 1:]
-                    if temp_gene in self.bank_set:
+                    if temp_gene in self.bank_set and temp_gene != gene:
+                        self.adjacency_list[gene].add(temp_gene)
 
+        self.prev_genes = set()
+        self.prev_genes.add(tuple(startGene))
+        self.most_recent_prev_genes = [startGene]
+        self.current_genes = set()
+        self.iteration = 0
+        while True:
+            self.iteration += 1
+
+            for gene in self.most_recent_prev_genes:
+                for gene_adj in self.adjacency_list[gene]:
+                    self.current_genes.add(gene_adj)
+
+            if endGene in self.current_genes:
+                return self.iteration
+            elif self.current_genes in self.prev_genes:
+                return -1
+            else:
+                self.prev_genes.add(list(self.current_genes))
+                self.most_recent_prev_genes = list(self.current_genes)
+                self.current_genes = set()
+                
 
 if __name__ == '__main__':
     test_cases = (["AACCGGTT", "AACCGGTA", ["AACCGGTA"], 1], ["AACCGGTT", "AAACGGTA", ["AACCGGTA","AACCGCTA","AAACGGTA"], 2])
