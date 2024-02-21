8d9a31015584f71dcf0e0558af3583d23ed39d10
cjremmett <cjremmett@gmail.com>
2024-02-21 17:47:40 -0500

    some work on 211

--- 211.py
+++ 211.py
@@ -6,11 +6,25 @@ class WordDictionary:
 
     def addWord(self, word: str) -> None:
         self.words.add(word)
-        
+        for i in range(0, len(word)):
+            for j in range(0, len(word)):
+                word_with_dots = word
+                word_with_dots = word_with_dots[0:i] + '.' + word_with_dots[i+1:]
+                word_with_dots = word_with_dots[0:j] + '.' + word_with_dots[j+1:]
+                self.words_with_dots.add(word_with_dots)
 
     def search(self, word: str) -> bool:
-        return True
+        if word in self.words or word in self.words_with_dots:
+            return True
+        else:
+            return False
         
 
 if __name__ == '__main__':
-    pass
+    trie = WordDictionary()
+    trie.addWord('apple')
+    print(trie.search('apple'))
+    print(trie.search('ap..e'))
+    print(trie.search('ap.l.'))
+    print(trie.search('appledfdfds'))
+    print(trie.search('a..'))
