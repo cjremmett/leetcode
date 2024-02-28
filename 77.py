58ec7d39241ee75064519e942fd08536a5a0a3db
cjremmett <cjremmett@gmail.com>
2024-02-28 18:28:40 -0500

    some work on 77.py

--- 77.py
+++ 77.py
@@ -2,13 +2,9 @@ from typing import List
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        self.return_set = set()
-        self.nums = set(range(1, n + 1))
-
-    
-    def iterate(self, progress_so_far, iteration_count):
-        if iteration_count >= k:
-            self.return_set.add(tuple(progress_so_far))
+        self.return_list = []
+        for i in range(1, n - 1):
+            for k in range(0, k):
 
 if __name__ == '__main__':
     test_cases = ([4, 2, [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]], [1, 1, [[1]]])
