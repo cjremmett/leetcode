bf4560da8039cdaf24b10233a047b295e6ca025c
cjremmett <cjremmett@gmail.com>
2024-03-07 08:37:57 -0500

    some work on 39

--- 39.py
+++ 39.py
@@ -3,7 +3,25 @@ from typing import List
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         self.return_list = []
-        for 
+        for i in range(0, len(candidates)):
+            self.iterate_num(c, cur_cand, candidates)
+
+        
+
+    def iterate_num(self, progress, to_add, sum, candidates):
+        if len(self.return_list) >= 150:
+            return
+
+        progress = progress + [to_add]
+        sum += to_add
+        
+        if sum > target:
+            return
+        elif sum == target:
+            self.return_list.append(progress)
+        else:
+            for i in range(0, len(candidates)):
+                self.iterate_num(progress, candidate, sum, candidates[i:])
     
 if __name__ == '__main__':
     test_cases = [ ([2,3,6,7], 7, [[2,2,3],[7]]), ([2,3,5], 8, [[2,2,2,2],[2,3,3],[3,5]]), ([2], 1, []) ]
