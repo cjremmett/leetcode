75d64eaff7ed886da26e79563ed30958786b0fd8
cjremmett <cjremmett@gmail.com>
2024-03-07 16:05:37 -0500

    finished 39

--- 39.py
+++ 39.py
@@ -4,14 +4,10 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         self.return_list = []
         for i in range(0, len(candidates)):
-            self.iterate_num(c, cur_cand, candidates)
+            self.iterate_num([], candidates[i], 0, candidates[i:], target)
+        return self.return_list
 
-        
-
-    def iterate_num(self, progress, to_add, sum, candidates):
-        if len(self.return_list) >= 150:
-            return
-
+    def iterate_num(self, progress, to_add, sum, candidates, target):
         progress = progress + [to_add]
         sum += to_add
         
@@ -21,9 +17,9 @@ class Solution:
             self.return_list.append(progress)
         else:
             for i in range(0, len(candidates)):
-                self.iterate_num(progress, candidate, sum, candidates[i:])
+                self.iterate_num(progress, candidates[i], sum, candidates[i:], target)
     
 if __name__ == '__main__':
     test_cases = [ ([2,3,6,7], 7, [[2,2,3],[7]]), ([2,3,5], 8, [[2,2,2,2],[2,3,3],[3,5]]), ([2], 1, []) ]
     for case in test_cases:
-        print('Input: ' + str(case[0]) + ' ' + str(case[1]) + '\nExpected output: ' + str(case[2]) + '\nActual output: ' + str(Solution().combinationSum(case[0], case[1])))
+        print('Input: ' + str(case[0]) + ' ' + str(case[1]) + '\nExpected output: ' + str(case[2]) + '\nActual output: ' + str(Solution().combinationSum(case[0], case[1])) + '\n')
