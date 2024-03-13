f363867641e1eb96a248fc9d42c80a845086f907
cjremmett <cjremmett@gmail.com>
2024-03-13 08:36:26 -0400

    some work on 22

--- 22.py
+++ 22.py
@@ -2,22 +2,19 @@ from typing import List
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        self.return_set = set()
-        self.iterate_parenthesis([], 0, 1, n)
+        self.return_set = set('()')
+        for i in range(1, n):
+            self.iterate_parenthesis()
         return self.return_set
 
-    def iterate_parenthesis(self, progress, insert_pos_one, insert_pos_two, n):
-        progress.insert(insert_pos_one, '(')
-        progress.insert(insert_pos_two, ')')
-        if len(progress) >= n * 2:
-            self.return_set.add(''.join(progress))
-        else:
-            for i in range(0, len(progress)):
-                for j in range(i + 1, len(progress) + 1):
-                    self.iterate_parenthesis(progress[:], i, j, n)
-    
+    def iterate_parenthesis(self):
+        results = self.return_set.copy()
+        for result in results:
+            self.return_set.add('()' + result)
+            self.return_set.add(result + '()')
+            self.return_set.add('(' + result + ')')
 
 if __name__ == '__main__':
-    test_cases = ( [7, ['']], [3, ["((()))","(()())","(())()","()(())","()()()"]], [1, ["()"]])
+    test_cases = ( [3, ["((()))","(()())","(())()","()(())","()()()"]], [2, ['()()', '(())']], [1, ["()"]])
     for case in test_cases:
         print('Input: ' + str(case[0]) + '\nExpected Output: ' + str(case[1]) + '\nActual Output: ' + str(Solution().generateParenthesis(case[0])) + '\n')
