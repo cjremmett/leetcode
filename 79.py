920b7b82d81a25b5b3b2c223baee39b3fd441571
cjremmett <cjremmett@gmail.com>
2024-03-14 08:27:27 -0400

    added 79

---
+++ 79.py
@@ -0,0 +1,18 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        self.positions_dict = defaultdict(set)
+        for i in range(0, len(board)):
+            for j in range(0, len(board[i])):
+                self.positions_dict[board[i][j]].add((i, j))
+
+        print(self.positions_dict)
+        return True
+
+
+if __name__ == '__main__':
+    test_cases = ( ([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], 'ABCCED', True), ([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], 'SEE', True), ([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], 'ABCB', False))
+    for case in test_cases:
+        print('Input: ' + str(case[0]) + ' ' + str(case[1]) + '\nExpected Output: ' + str(case[2]) + '\nActual Output: ' + str(Solution().exist(case[0], case[1])) + '\n')
