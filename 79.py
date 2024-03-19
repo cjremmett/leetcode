5b79551df1ba43f3d339434462e633f4c2e45c67
cjremmett <cjremmett@gmail.com>
2024-03-19 17:07:20 -0400

    finished 79

--- 79.py
+++ 79.py
@@ -9,7 +9,9 @@ class Solution:
                 self.positions_dict[board[i][j]].add((i, j))
 
         for tup in self.positions_dict[word[0]]:
-            result = self.next_letter_iteration((tup[0], tup[1]), word, 1, set())
+            already_seen = set()
+            already_seen.add(tup)
+            result = self.next_letter_iteration(tup, word, 1, already_seen)
             if result == True:
                 return True
         
@@ -28,21 +30,21 @@ class Solution:
 
         if (pos[0] + 1, pos[1]) in self.positions_dict[word[count]] and (pos[0] + 1, pos[1]) not in already_seen:
             down_seen = already_seen.copy()
-            down_seen.add((pos[0] - 1, pos[1]))
+            down_seen.add((pos[0] + 1, pos[1]))
             down = self.next_letter_iteration((pos[0] + 1, pos[1]), word, count + 1, down_seen)
             if down == True:
                 return True
 
         if (pos[0], pos[1] - 1) in self.positions_dict[word[count]] and (pos[0], pos[1] - 1) not in already_seen:
             left_seen = already_seen.copy()
-            left_seen.add((pos[0] - 1, pos[1]))
+            left_seen.add((pos[0], pos[1] - 1))
             left = self.next_letter_iteration((pos[0], pos[1] - 1), word, count + 1, left_seen)
             if left == True:
                 return True
 
         if (pos[0], pos[1] + 1) in self.positions_dict[word[count]] and (pos[0], pos[1] + 1) not in already_seen:
             right_seen = already_seen.copy()
-            right_seen.add((pos[0] - 1, pos[1]))
+            right_seen.add((pos[0], pos[1] + 1))
             right = self.next_letter_iteration((pos[0], pos[1] + 1), word, count + 1, right_seen)
             if right == True:
                 return True
@@ -53,6 +55,6 @@ class Solution:
 
 
 if __name__ == '__main__':
-    test_cases = ( ([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], 'ABCB', False), ([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], 'ABCCED', True), ([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], 'SEE', True))
+    test_cases = ( ([["a","a"]], 'aaa', False), ([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], 'ABCB', False), ([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], 'ABCCED', True), ([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], 'SEE', True))
     for case in test_cases:
         print('Input: ' + str(case[0]) + ' ' + str(case[1]) + '\nExpected Output: ' + str(case[2]) + '\nActual Output: ' + str(Solution().exist(case[0], case[1])) + '\n')
