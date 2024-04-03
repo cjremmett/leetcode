9033cfc84e0f10bbe35d3a7fa71d2b4492579675
cjremmett <cjremmett@gmail.com>
2024-04-03 08:33:42 -0400

    some work on 162

--- 162.py
+++ 162.py
@@ -1,22 +1,34 @@
-from typing import List
+from typing import List, Optional
 
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         left_pointer = 0
         right_pointer = len(nums) - 1
+        if self.check_is_peak(left_mid, nums):
+                return left_mid
+        if self.check_is_peak(right_mid, nums):
+            return right_mid
 
         while True:
-            mid_index = (right_pointer - left_pointer) // 2
-            mid_greater_than_left = True if (mid_index == 0 or nums[mid_index] > nums[mid_index - 1]) else False
-            mid_greater_than_right = True if (mid_index == len(nums) - 1 or nums[mid_index] > nums[mid_index + 1]) else False
-            if mid_greater_than_left and mid_greater_than_right:
-                return mid_index
+            left_mid = (right_pointer - left_pointer) // 3
+            right_mid = ((right_pointer - left_pointer) // 3) * 2
+            if self.check_is_peak(left_mid, nums):
+                return left_mid
+            if self.check_is_peak(right_mid, nums):
+                return right_mid
+
+            if nums[left_mid] == nums[right_mid]:
+                left_pointer, right_pointer = left_mid, right_mid
+            elif nums[left_mid] > nums[right_mid]:
+                right_pointer = left_mid
             else:
-                if nums[left_pointer] > nums[right_pointer]:
-                    right_pointer = mid_index
-                else:
-                    left_pointer = mid_index
-    
+                left_pointer = right_mid
+
+
+    def check_is_peak(self, index, nums):
+        greater_than_left = True if (index == 0 or nums[index] > nums[index - 1]) else False
+        greater_than_right = True if (index == len(nums) - 1 or nums[index] > nums[index + 1]) else False
+        return True if greater_than_left and greater_than_right else False
 
 if __name__ == '__main__':
     test_cases = ( ([1,2,3,1], [2]), ([1,2,1,3,5,6,4], [1, 5]))
