402f0e9ebdc0c538a5de5d596c1a6f63df5af19e
cjremmett <cjremmett@gmail.com>
2024-04-23 08:33:28 -0400

    some work on 373

--- 373.py
+++ 373.py
@@ -6,13 +6,15 @@ class Solution:
         heap = []
         for i in range(0, len(nums1)):
             for j in range(0, len(nums2)):
-                if len(heap) < k or nums1[i] + nums2[j] <= (heap[0][0] + heap[0][1]):
-                    heapq.heappush(heap, [nums1[i], nums2[j]])
-                    heap = heapq.nlargest(k, heap, key= lambda x: -1 * (x[0] + x[1]))
+                heap = heapq.merge(heap, [[nums1[i], nums2[j]]], key=lambda x: x[0] + x[1])
         
-        return heap
+        returnlist = []
+        for item in heap:
+            returnlist.append(item)
+
+        return returnlist
     
 if __name__ == '__main__':
-    test_cases = ( ([1,7,11], [2,4,6], 3, [[1,2],[1,4],[1,6]]), ([1,1,2], [1,2,3], 2, [[1,1],[1,1]]))
+    test_cases = ( ([1,2,4,5,6], [3,5,7,9], 3, [[1,3],[2,3],[1,5]]), ([1,7,11], [2,4,6], 3, [[1,2],[1,4],[1,6]]), ([1,1,2], [1,2,3], 2, [[1,1],[1,1]]))
     for case in test_cases:
         print('Input: ' + str(case[0]) + ', ' + str(case[1]) + ', ' + str(case[2]) + '\nExpected output: ' + str(case[3]) + '\nActual output: ' + str(Solution().kSmallestPairs(case[0], case[1], case[2])) + '\n')
