5cff8bae8faebe28dcd0a8a00431d3cb6d54504f
cjremmett <cjremmett@gmail.com>
2024-04-25 08:32:57 -0400

    Some work on 373

--- 373.py
+++ 373.py
@@ -6,13 +6,25 @@ class Solution:
         heap = []
         for i in range(0, len(nums1)):
             for j in range(0, len(nums2)):
-                heap = heapq.merge(heap, [[nums1[i], nums2[j]]], key=lambda x: x[0] + x[1])
-        
-        returnlist = []
-        for item in heap:
-            returnlist.append(item)
+                sum = nums1[i] + nums2[j]
+                if len(heap) < k:
 
-        return returnlist
+
+    def binary_insert(self, array, element):
+        left = 0
+        right = len(array)
+        while True:
+            
+
+    def should_insert(self, array, element_sum, index):
+        if index == 0 and element_sum >= array[0][0] + array[0][1]:
+            return True
+        elif index = len(array) and element_sum <= array[-1][0] + array[-1][1]:
+            return True
+        elif element_sum >= array[index][0] + array[index][1] and element_sum <= array[index - 1][0] + array[index - 1][1]:
+            return True
+        else:
+            return False
     
 if __name__ == '__main__':
     test_cases = ( ([1,2,4,5,6], [3,5,7,9], 3, [[1,3],[2,3],[1,5]]), ([1,7,11], [2,4,6], 3, [[1,2],[1,4],[1,6]]), ([1,1,2], [1,2,3], 2, [[1,1],[1,1]]))
