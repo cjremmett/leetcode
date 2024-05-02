37d613b1c51de75186fc6b3ecce6357de5231aad
cjremmett <cjremmett@gmail.com>
2024-05-02 08:30:31 -0400

    some work on 172

--- 172.py
+++ 172.py
@@ -4,14 +4,17 @@ class Solution:
     def trailingZeroes(self, n: int) -> int:
         count = 0
         nfac = math.factorial(n)
-        nfac = str(nfac)
-        for i in reversed(range(0, len(nfac))):
-            if nfac[i] == '0':
+        while True:
+            if nfac % 10 == 0:
                 count += 1
+                nfac = nfac // 10
             else:
                 return count
+            
 
 if __name__ == '__main__':
-    test_cases = ( (3, 0), (5, 1), (0, 0))
+    num = 1234
+    first_digit = num 
+    test_cases = ( (3, 0), (5, 1), (0, 0), (4830, 0))
     for case in test_cases:
         print('Input: ' + str(case[0]) + '\nExpected output: ' + str(case[1]) + '\nActual output: ' + str(Solution().trailingZeroes(case[0])) + '\n')
