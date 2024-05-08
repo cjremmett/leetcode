5945a8410c18b5e2ec3cd3e3cd0cd60b94e96882
cjremmett <cjremmett@gmail.com>
2024-05-08 17:36:24 -0400

    finished 139

--- 139.py
+++ 139.py
@@ -4,18 +4,20 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         max_size = max(map(len, wordDict))
         segment_indices = set()
-        for i in range(0, len(s)):
+        segment_indices.add(0)
+        for i in range(1, len(s) + 1):
             window = set()
             for j in range(1, max_size + 1):
                 window.add(max(i-j, 0))
             
             for index in window:
                 if index in segment_indices:
-                    if s[index:i + 1] in wordDict:
+                    debug = s[index:i]
+                    if s[index:i] in wordDict:
                         segment_indices.add(i)
                         break
         
-        if len(s) - 1 in segment_indices:
+        if len(s) in segment_indices:
             return True
         else:
             return False
