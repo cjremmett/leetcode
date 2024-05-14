7f7e4d8c4bbea23cc8b3b302de723f425d12a1ed
cjremmett <cjremmett@gmail.com>
2024-05-14 16:40:09 -0400

    finished 322

--- 322.py
+++ 322.py
@@ -2,32 +2,30 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if len(coins) == 0 or amount == 0 or coins[0] > amount:
+        if amount == 0:
+            return 0
+        elif len(coins) == 0:
             return -1
 
-        if amount % coins[-1] == 0:
-            return amount // coins[-1]
-        else:
-            segment_amount = amount % coins[-1]
-            while segment_amount >= 0:
-                result = self.check_segment
-                
-    def check_segment(self, coins, target, count):
-        if len(coins) == 0 or coins[0] > target:
-            return -1
-        elif target % coins[-1] == 0:
-            return count + target // coins[-1]
-        else:
-            result = self.check_segment(coins[:-1], target - coins[-1], count)
-            if result == -1:
+        values = set()
+        values.update(coins)
+        if amount in values:
+            return 1
+        i = 1
+        while True:
+            iteration_values = values.copy()
+            i += 1
+            if len(iteration_values) == 0:
                 return -1
-            else:
-                return count + result
-
-        
-
+            for segment in iteration_values:
+                for coin in coins:
+                    if segment + coin == amount:
+                        return i
+                    elif segment + coin < amount:
+                        values.add(segment + coin)
+                values.discard(segment)
 
 if __name__ == '__main__':
-    test_cases = ( ([3,5], 9, 3), ([1,2,5], 11, 3), ([2], 3, -1), ([1], 0, 0))
+    test_cases = ( ([474,83,404,3], 264, 8), ([3,5], 9, 3), ([1,2,5], 11, 3), ([2], 3, -1), ([1], 0, 0), ([1], 1, 1))
     for case in test_cases:
         print('Input: ' + str(case[0]) + ', ' + str(case[1]) + '\nExpected output: ' + str(case[2]) + '\nActual output: ' + str(Solution().coinChange(case[0], case[1])) + '\n')
