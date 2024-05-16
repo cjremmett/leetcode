e94d4b20a55955aa888b1e5a94c08af117be5693
cjremmett <cjremmett@gmail.com>
2024-05-16 08:33:59 -0400

    some work on 5

--- 5.py
+++ 5.py
@@ -5,7 +5,7 @@ class Solution:
             left = i
             right = i
             while True:
-                if left - 1 >= 0 and right + 1 < len(s) and s[i-left] == s[i+right]:
+                if left - 1 >= 0 and right + 1 < len(s) and s[left - 1] == s[right + 1]:
                     left -= 1
                     right += 1
                 elif right - left + 1 > len(max_palindrome):
@@ -14,9 +14,22 @@ class Solution:
                 else:
                     break
 
+        for i in range(1, len(s)):
+            left = i - 1
+            right = i
+            while True:
+                if left - 1 >= 0 and right + 1 < len(s) and s[left-1] == s[right+1]:
+                    left -= 1
+                    right += 1
+                elif right - left + 1 > len(max_palindrome) and s[left:right+1] == s[left:right+1][::-1]:
+                    max_palindrome = s[left:right+1]
+                    break
+                else:
+                    break
+
         return max_palindrome
     
 if __name__ == '__main__':
-    test_cases = ( ("babad", ['bab', 'aba']), ("cbbd", ['bb']))
+    test_cases = ( ("cbbd", ['bb']), ("babad", ['bab', 'aba']))
     for case in test_cases:
         print('Input: ' + case[0] + '\nExpected output: ' + str(case[1]) + '\nActual output: ' + Solution().longestPalindrome(case[0]) + '\n')
