6a5e6dd1cb16ab8ba2346f8a2a464bf764c361b1
cjremmett <cjremmett@gmail.com>
2024-05-29 11:22:06 -0400

    finished 1404

--- 1404.py
+++ 1404.py
@@ -4,9 +4,17 @@ class Solution:
         for i in range(0, len(s)):
             decimal_num += int(s[len(s) - 1 -  i]) * (2 ** i)
 
-        return decimal_num
+        i = 0
+        while decimal_num != 1:
+            i += 1
+            if decimal_num % 2 == 0:
+                decimal_num = decimal_num // 2
+            else:
+                decimal_num = decimal_num + 1
+
+        return i
 
 if __name__ == '__main__':
-    test_cases = ( ("1101", 6), ("10", 1))
+    test_cases = ( ("1111011110000011100000110001011011110010111001010111110001", 85), ("1101", 6), ("10", 1))
     for case in test_cases:
         print('Input: ' + str(case[0]) + '\nExpected output: ' + str(case[1]) + '\nActual output: ' + str(Solution().numSteps(case[0])) + '\n')
