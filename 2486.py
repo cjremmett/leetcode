e944bddd45a11ebbcae73b2265ee15075d26ff4d
cjremmett <cjremmett@gmail.com>
2024-06-05 16:35:32 -0400

    some work on 2486

--- 2486.py
+++ 2486.py
@@ -1,28 +1,32 @@
-from collections import defaultdict
-
 class Solution:
     def appendCharacters(self, s: str, t: str) -> int:
-        indexes = defaultdict(list)
+        indexes = {}
         for i in range(0, len(s)):
-            indexes[s[i]].append(i)
+            if s[i] in indexes:
+                indexes[s[i]].append(i)
+            else:
+                indexes[s[i]] = [i]
 
         recent_pair = tuple()
         for i in range(0, len(t)):
-            if len(recent_pair) == 0 and len(indexes[t[i]]) != 0:
+            if len(recent_pair) == 0 and t[i] in indexes:
                 recent_pair = (t[i], indexes[t[i]][0])
             else:
                 if t[i] in indexes:
                     for j in range(0, len(indexes[t[i]])):
-                        if indexes[t[i]] > recent_pair[1]:
+                        if indexes[t[i]][j] > recent_pair[1]:
                             recent_pair = (t[i], indexes[t[i]][j])
+                            indexes[t[i]] = indexes[t[i]][j+1:]
                             break
-
-                if len(recent_pair) > 0 and recent_pair[0] != t[i]:
+                else:
                     return len(t[i:])
 
+        if len(recent_pair) > 0:
             return 0
+        else:
+            return len(t)
 
 if __name__ == '__main__':
-    test_cases = ( ("coaching", "coding", 4), ("abcde", "a", 0), ("z", "abcde", 5))
+    test_cases = ( ("zbc", "abc", 3), ("coaching", "coding", 4), ("abcde", "a", 0), ("z", "abcde", 5))
     for case in test_cases:
         print('Input: ' + case[0] + ' ' + case[1] + '\nExpected output: ' + str(case[2]) + '\nActual output: ' + str(Solution().appendCharacters(case[0], case[1])) + '\n')
