71ae902cfa51c3c48a2dddb0a5f0aae2ac1cde4c
cjremmett <cjremmett@gmail.com>
2024-06-11 07:50:26 -0400

    finished 846

--- 846.py
+++ 846.py
@@ -2,21 +2,35 @@ from typing import List
 
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        group_array = []
+        if groupSize == 1:
+            return True
+        group = []
+        index = {}
         hand = sorted(hand)
+        num_incomplete_groups = 0
 
         for num in hand:
-            placed = False
-            for group in group_array:
-                if len(group) < groupSize and group[-1] == num - 1:
-                    group.append(num)
-                    placed = True
-                    break
-            
-            if placed == False:
-                group_array.append([num])
+            if num in index and len(index[num]) > 0:
+                group_index = index[num][-1]
+                group[group_index].append(num)
+                if len(group[index[num][-1]]) < groupSize:
+                    if num + 1 in index:
+                        index[num+1].append(group_index)
+                    else:
+                        index[num+1] = [group_index]
+                else:
+                    num_incomplete_groups -= 1
+                index[num].pop()
+            else:
+                group.append([num])
+                num_incomplete_groups += 1
+                if num + 1 in index:
+                    index[num+1].append(len(group) - 1)
+                else:
+                    index[num+1] = [len(group) - 1]
 
-        if min(map(lambda x: len(x), group_array)) == groupSize:
+        #if min(map(lambda x: len(x), group)) == groupSize:
+        if num_incomplete_groups == 0:
             return True
         else:
             return False
