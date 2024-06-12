e31a9b1344e13fc85653f8abf24425eff778e182
cjremmett <cjremmett@gmail.com>
2024-06-12 07:15:10 -0400

    finished 648

--- 648.py
+++ 648.py
@@ -2,7 +2,16 @@ from typing import List
 
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        dictset = set(dictionary)
+        dictionary = set(dictionary)
+
+        sentence = sentence.split(' ')
+        for w in range(0, len(sentence)):
+            for i in range(1, len(sentence[w])):
+                if sentence[w][:i] in dictionary:
+                    sentence[w] = sentence[w][:i]
+
+        s = ' '
+        return s.join(sentence)
 
 if __name__ == '__main__':
     test_cases = ( (["cat","bat","rat"], "the cattle was rattled by the battery", "the cat was rat by the bat"), (["a","b","c"], "aadsfasf absbs bbab cadsfafs", "a a b c"))
