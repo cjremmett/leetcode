fb8462a8ba0b156b43207ae6b997942f8dbef9c9
cjremmett <cjremmett@gmail.com>
2024-07-17 07:29:07 -0400

    finished 1382

--- 1382.py
+++ 1382.py
@@ -14,23 +14,7 @@ class Solution:
         left_node = None
         right_node = None
 
-        prev_node = None
-        for i in range(0, root_index):
-            new_node = TreeNode(inorder_array[i], None, None)
-            if prev_node:
-                new_node.left = prev_node
-            prev_node = new_node
-        left_node = prev_node
-
-        prev_node = None
-        for i in reversed(range(root_index + 1, len(inorder_array))):
-            new_node = TreeNode(inorder_array[i], None, None)
-            if prev_node:
-                new_node.right = prev_node
-            prev_node = new_node
-        right_node = prev_node
-
-        return TreeNode(inorder_array[root_index], left_node, right_node)
+        return self.return_next_node(inorder_array)
 
 
     def load_array(self, inorder_array, node):
@@ -38,6 +22,17 @@ class Solution:
             self.load_array(inorder_array, node.left)
             inorder_array.append(node.val)
             self.load_array(inorder_array, node.right)
+
+    def return_next_node(self, values_to_create):
+        if len(values_to_create) == 0:
+            return None
+        elif len(values_to_create) == 1:
+            return TreeNode(values_to_create[0], None, None)
+        else:
+            mid = len(values_to_create) // 2
+            left = self.return_next_node(values_to_create[0:mid])
+            right = self.return_next_node(values_to_create[mid+1:])
+            return TreeNode(values_to_create[mid], left, right)
 
 
 def inorder_print(node):
@@ -53,7 +48,9 @@ def preorder_print(node):
         preorder_print(node.right)
 
 if __name__ == '__main__':
-    node4 = TreeNode(4, None, None)
+    node6 = TreeNode(6, None, None)
+    node5 = TreeNode(5, None, node6)
+    node4 = TreeNode(4, None, node5)
     node3 = TreeNode(3, None, node4)
     node2 = TreeNode(2, None, node3)
     node1 = TreeNode(1, None, node2)
