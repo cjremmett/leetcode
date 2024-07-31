031d3462d29934c04a24d74db45bc51eeaf54dba
cjremmett <cjremmett@gmail.com>
2024-07-31 07:20:36 -0400

    finished 1509

--- 1509.py
+++ 1509.py
@@ -7,16 +7,14 @@ class Solution:
             return 0
 
         nums.sort()
-        for i in range(0, 3):
-            if nums[1] - nums[0] > nums[-1] - nums[-2]:
-                nums.pop(0)
-            else:
-                nums.pop()
+        min_dif = nums[-1] - nums[0]
+        for i in range(0, 4):
+            min_dif = min(min_dif, nums[i - 4] - nums[i])
 
-        return nums[-1] - nums[0]
+        return min_dif
         
     
 if __name__ == '__main__':
-    test_cases = ( ([5,3,2,4], 0), ([1,5,0,10,14], 1))
+    test_cases = ( ([6,6,0,1,1,4,6], 2), ([5,3,2,4], 0), ([1,5,0,10,14], 1))
     for case in test_cases:
         print('Input: ' + str(case[0]) + '\nExpected output: ' + str(case[1]) + '\nActual ouput: ' + str(Solution().minDifference(case[0])) + '\n')
