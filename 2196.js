f01d198014db6474de8251c10fbe21bfbb2d1cc0
cjremmett <cjremmett@gmail.com>
2024-09-10 08:49:00 -0400

    some work on 2196

---
+++ 2196.js
@@ -0,0 +1,17 @@
+
+function TreeNode(val, left, right) 
+{
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
+/**
+ * @param {number[][]} descriptions
+ * @return {TreeNode}
+ */
+var createBinaryTree = function(descriptions) 
+{
+    return "abc"
+};
+
