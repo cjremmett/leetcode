031b7d4c2eb9062dfff645ed2c38db85bf9f7494
cjremmett <cjremmett@gmail.com>
2024-12-10 17:34:34 -0500

    finished 1280

--- 1280.sql
+++ 1280.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select studs.student_id, studs.student_name, subs.subject_name, ed.attended_exams from Students studs
+select studs.student_id, studs.student_name, subs.subject_name, coalesce(ed.attended_exams, 0) as "attended_exams" from Students studs
 left join Subjects subs on 1 = 1
 left join (select e.student_id, e.subject_name, count(*) as "attended_exams" from Examinations e group by e.student_id, e.subject_name) ed on ed.student_id = studs.student_id and ed.subject_name = subs.subject_name
