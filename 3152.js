78418f9dca0c5d2c2ab6093060eef3a883677b40
cjremmett <cjremmett@gmail.com>
2024-12-17 08:01:47 -0500

    finished 3152

--- 3152.js
+++ 3152.js
@@ -38,23 +38,21 @@ let isArraySpecial = function(nums, queries)
  */
 let checkQuery = function(failIndexes, query)
 {
-    console.log(failIndexes);
     let left = 0;
     let right = failIndexes.length - 1;
     while(true)
     {
         let mid = Math.floor(left + ((right - left) / 2));
-        console.log(mid);
 
         if(failIndexes[mid] >= query[0] && failIndexes[mid] < query[1])
         {
             return false;
         }
-        else if(left >= right)
+        else if(left > right)
         {
             return true;
         }
-        else if(mid < query[0])
+        else if(failIndexes[mid] < query[0])
         {
             left = mid + 1;
         }
@@ -65,7 +63,7 @@ let checkQuery = function(failIndexes, query)
     }
 };
 
-let testCases = [ [[3,4,1,2,6], [[0,4]], [false]], [[4,3,1,6], [[0,2],[2,3]], [false, true]] ];
+let testCases = [ [[9,8,7,1,5,9], [[2,3]], false], [[3,4,1,2,6], [[0,4]], [false]], [[4,3,1,6], [[0,2],[2,3]], [false, true]] ];
 
 for(let i = 0; i < testCases.length; i++)
 {
